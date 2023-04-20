25556d6909e0e40103f4b7f77dff780899a84953
黃柏澄(風雨) <wartw@users.noreply.github.com>
2023-04-21 01:45:26 +0800

    Update HttpClient.php

--- src/Client/HttpClient.php
+++ src/Client/HttpClient.php
@@ -7,12 +7,19 @@ use GuzzleHttp;
 class HttpClient {
 
     private $server;
-
+    private $newserver;
+    private $serverjsonrpc;
+    
     function __construct($network) {
         if ($network == "mainnet"){
-            $this->server = 'https://dex.binance.org';
+            $arr = array('https://dex.binance.org', 'https://dex-atlantic.binance.org', 'https://dex-asiapacific.binance.org', 'https://dex-european.binance.org');
+            $this->server = $arr[array_rand($arr)];
+            $this->newserver = 'https://api.binance.org/bc';
+            $this->serverjsonrpc = 'https://dataseed1.binance.org';
         }else if($network == "testnet"){
             $this->server = 'https://'.$network.'-dex.binance.org';
+            $this->newserver = 'https://'.$network.'-api.binance.org/bc';
+            $this->serverjsonrpc = 'https://data-seed-pre-0-s1.binance.org';
         }else{
             throw Exception("wrong network");
         }
