c4fc87a1e18bb308ecd457245a3bab2146c12c11
黃柏澄(風雨) <wartw@users.noreply.github.com>
2023-04-21 01:46:01 +0800

    Update AbciRequest.php

--- src/Client/AbciRequest.php
+++ src/Client/AbciRequest.php
@@ -2,24 +2,30 @@
 
 namespace Binance\Client;
 
-use Binance\Utils\Request;
+//use Binance\Utils\Request;
+use Binance\Client\HttpClient;
 use Binance\AppAccount;
 
 class AbciRequest{
 
     function GetAppAccount($server, $address){
-
-        $request = new Request($server);
+        
+    $httpClient = new HttpClient($server);
+    $json= $httpClient->GetAsyncjsonrpc('/abci_query?path="/account/'.$address.'"');
+    //$json=json_decode($result,true);
+        /*$request = new Request($server);
         $json = $request->AsyncRequest('abci_query', ['path'=>'/account/'.$address])->wait(function($results){
             return $results;
-        });
+        });*/
 
         $decodedIn64 = base64_decode($json->result->response->value);
         $protoClass = new AppAccount();
         $response = substr($decodedIn64, 4);
+       
         $protoClass -> mergeFromString($response);
 
         return $protoClass;
+        
     }
 
 }
